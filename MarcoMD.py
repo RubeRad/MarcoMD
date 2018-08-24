45bcda3a0ca9a51adeff7e143ec3b65e669b4930
Reuben Settergren <ruberad@gmail.com>
2018-08-24 14:30:55 -0700

    Add new unguent when previous comes to rest. End game when new unguent is blocked

--- MarcoMD.py
+++ MarcoMD.py
@@ -11,21 +11,28 @@ import events
 settings = Settings()
 pygame.init()
 screen = pygame.display.set_mode((settings.screenw, settings.screenh))
-bacteria = Group()
+statics = Group()
 for i in range(settings.n_bacteria):
-    bacteria.add(Bacterium(settings, screen, bacteria))
+    statics.add(Bacterium(settings, screen, statics))
 u = Unguent(settings, screen)
 
 while True:
     # take care of bidnis
     events.handle(settings, u)
-    if pygame.sprite.spritecollide(u, bacteria, False):
+    if pygame.sprite.spritecollide(u, statics, False):
         u.moving_down = False
-    u.update(bacteria)
+    u.update(statics)
+    if not u.moving_down: # done moving! time for a new one
+        statics.add(u)
+        u = Unguent(settings, screen)
+        # is the new unguent blocked from entering the screen?
+        if pygame.sprite.spritecollide(u, statics, False):
+            print("Kablooey")
+            break; # game over
 
     # redraw everything
     screen.fill(settings.bg_color)
-    for b in bacteria.sprites():
+    for b in statics.sprites():
         b.render()
     u.render()
     pygame.display.flip()
